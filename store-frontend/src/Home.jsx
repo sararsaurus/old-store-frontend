96a9fdd48567e787e15745da5bd2163faac17e98
Sara Raley <sara.m.raley@gmail.com>
2023-04-07 11:13:52 -0600

    imports axios and hooks

--- store-frontend/src/Home.jsx
+++ store-frontend/src/Home.jsx
@@ -1,6 +1,20 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { ProductsIndex } from "./ProductsIndex";
 
 export function Home() {
+  const [products, setProducts] = useState([]);
+
+  const handleIndexProducts = () => {
+    console.log("TESTING INDEX PRODUCTS");
+    axios.get("http://localhost:3000/products.json").then((response) => {
+      console.log(response.data);
+      setProducts(response.data);
+    });
+  };
+
+  useEffect(handleIndexProducts, []);
+
   return (
     <div>
       <h1>Welcome to React!</h1>
